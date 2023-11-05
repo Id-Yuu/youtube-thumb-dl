1a147d00288e7543d2881d4e3b92e3d398221a68
Yuu <122996864+Id-Yuu@users.noreply.github.com>
2023-11-05 23:11:23 +0700

    refactorJs

--- main.js
+++ main.js
@@ -1,59 +1,55 @@
-const formsData = (e) => {
+const formsData = async (e) => {
   e.preventDefault();
-  // get input
-  const url = document.getElementById("url").value;
-  // Check Error
-  if (url === "") {
-    alert("Please Enter URL");
+
+  // Get the input element and its value
+  const urlInput = document.getElementById("url");
+  const url = urlInput.value.trim();
+
+  // Check if the URL is valid
+  const validUrl = url.includes("watch?v=") && url.length >= 32;
+
+  // If the URL is not valid, show an alert and return
+  if (!validUrl) {
+    alert("Please enter a valid URL");
     return;
-  } else if (!url.includes("watch?v=")) {
-    alert("Please Enter Valid URL");
-    return;
-  } else if (url.length < 32) {
-    alert("Please Enter Valid URL");
-    return;
-  } else {
-    // get image
-    const params = url.slice(32);
-    const imageUrls = [
-      `https://img.youtube.com/vi/${params}/maxresdefault.jpg`, // High Res
-      `https://img.youtube.com/vi/${params}/sddefault.jpg`, // Standard
-      `https://img.youtube.com/vi/${params}/hqdefault.jpg`, // low
-    ];
-    // output
-    const outputTitle = document.getElementById("outputTitle");
-    let img = "";
-    imageUrls.forEach((url) => (img += `<img src="${url}">`));
-    outputTitle.insertAdjacentHTML(
-      "afterend",
-      "<div class='list-img'>" + img + "</div>"
-    );
-    // scroll
-    const listImg = document.querySelector(".list-img");
-    listImg.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-      scrollMode: "if-needed",
-    });
-    // clear
-    const reset = document.querySelector(".reset");
-    reset.addEventListener("click", () => {
-      url.value = "";
-    });
   }
-  // Back to top
+
+  // Extract the parameters from the URL
+  const params = url.slice(32);
+
+  // Define the image URLs for different resolutions
+  const imageUrls = [
+    `https://img.youtube.com/vi/${params}/maxresdefault.jpg`,
+    `https://img.youtube.com/vi/${params}/sddefault.jpg`,
+    `https://img.youtube.com/vi/${params}/hqdefault.jpg`,
+  ];
+
+  // Get the output title element and insert the images
+  const outputTitle = document.getElementById("outputTitle");
+  const img = imageUrls.map((url) => `<img src="${url}">`).join("");
+  outputTitle.insertAdjacentHTML(
+    "afterend",
+    `<div class='list-img'>${img}</div>`
+  );
+
+  // Scroll to the list of images
+  const listImg = document.querySelector(".list-img");
+  listImg.scrollIntoView({ behavior: "smooth", block: "start" });
+
+  // Add a click event listener to reset the form
+  const resetBtn = document.querySelector(".reset");
+  resetBtn.addEventListener("click", () => {
+    urlInput.value = "";
+  });
+
+  // Check if the page is scrolled and show the back to top button
   const height = document.documentElement.scrollHeight;
-  if (height > 1000) {
-    document.querySelector(".backToTop").style.display = "block";
-  }
-  const backToTop = document.querySelector(".backToTop");
-  backToTop.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
-    document.querySelector(".backToTop").style.display = "none";
+  const backToTopBtn = document.querySelector(".backToTop");
+  backToTopBtn.style.display = height > 1000 ? "block" : "none";
+
+  // Add a click event listener to scroll to the top
+  backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    backToTopBtn.style.display = "none";
   });
 };
