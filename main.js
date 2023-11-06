8876824ab237f4c6c7045147170bee3037a9670c
Yuu <122996864+Id-Yuu@users.noreply.github.com>
2023-11-06 15:27:03 +0700

    restore

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 const formsData = async (e) => {
   e.preventDefault();
 
+  // Get the input element and its value
   const urlInput = document.getElementById("url");
   const url = urlInput.value.trim();
 
-  if (!isValidUrl(url)) {
+  // Check if the URL is valid
+  const validUrl = url.includes("watch?v=") && url.length >= 32;
+
+  // If the URL is not valid, show an alert and return
+  if (!validUrl) {
     alert("Please enter a valid URL");
     return;
   }
 
-  const params = url.split("v=")[1];
+  // Extract the parameters from the URL
+  const params = url.slice(32);
+
+  // Define the image URLs for different resolutions
   const imageUrls = [
     `https://img.youtube.com/vi/${params}/maxresdefault.jpg`,
     `https://img.youtube.com/vi/${params}/sddefault.jpg`,
     `https://img.youtube.com/vi/${params}/hqdefault.jpg`,
   ];
 
+  // Get the output title element and insert the images
   const outputTitle = document.getElementById("outputTitle");
   const img = imageUrls.map((url) => `<img src="${url}">`).join("");
   outputTitle.insertAdjacentHTML(
@@ -23,33 +32,24 @@ const formsData = async (e) => {
     `<div class='list-img'>${img}</div>`
   );
 
+  // Scroll to the list of images
   const listImg = document.querySelector(".list-img");
   listImg.scrollIntoView({ behavior: "smooth", block: "start" });
 
+  // Add a click event listener to reset the form
   const resetBtn = document.querySelector(".reset");
   resetBtn.addEventListener("click", () => {
     urlInput.value = "";
   });
 
+  // Check if the page is scrolled and show the back to top button
   const height = document.documentElement.scrollHeight;
   const backToTopBtn = document.querySelector(".backToTop");
   backToTopBtn.style.display = height > 1000 ? "block" : "none";
 
+  // Add a click event listener to scroll to the top
   backToTopBtn.addEventListener("click", () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
     backToTopBtn.style.display = "none";
   });
-};
-
-const isValidUrl = (url) => {
-  const ifUrl = [
-    { key: "watch?v=", minLength: 32 },
-    { key: "m.youtube.com", minLength: 30 },
-    { key: "youtu.be", minLength: 23 },
-    { key: "youtube.com", minLength: 30 },
-  ];
-
-  return ifUrl.some(
-    ({ key, minLength }) => url.includes(key) && url.length >= minLength
-  );
 };
