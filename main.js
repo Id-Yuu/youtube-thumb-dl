04d87f0f491de1819f1a24311fdfbdecb4bb28c3
Yuu <122996864+Id-Yuu@users.noreply.github.com>
2023-11-06 15:44:57 +0700

    RefactorJsFixAnyUrl

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
 const formsData = async (e) => {
   e.preventDefault();
 
-  // Get the input element and its value
   const urlInput = document.getElementById("url");
   const url = urlInput.value.trim();
 
-  // Check if the URL is valid
-  const validUrl = url.includes("watch?v=") && url.length >= 32;
+  const isValidUrl = url.includes("watch?v=") && url.length >= 32;
+  const isMobileUrl =
+    url.includes("m.youtube.com/watch?v=") && url.length >= 37;
+  const isValidUrl2 = url.includes("youtube.com/watch?v=") && url.length >= 32;
 
-  // If the URL is not valid, show an alert and return
-  if (!validUrl) {
-    alert("Please enter a valid URL");
-    return;
+  const params = url.split("watch?v=")[1];
+
+  if (isMobileUrl || isValidUrl2) {
+    params.split("&")[0];
+  } else if (!isValidUrl) {
+    alert("Invalid URL");
   }
 
-  // Extract the parameters from the URL
-  const params = url.slice(32);
-
-  // Define the image URLs for different resolutions
   const imageUrls = [
     `https://img.youtube.com/vi/${params}/maxresdefault.jpg`,
     `https://img.youtube.com/vi/${params}/sddefault.jpg`,
     `https://img.youtube.com/vi/${params}/hqdefault.jpg`,
   ];
 
-  // Get the output title element and insert the images
   const outputTitle = document.getElementById("outputTitle");
   const img = imageUrls.map((url) => `<img src="${url}">`).join("");
   outputTitle.insertAdjacentHTML(
@@ -32,22 +30,18 @@ const formsData = async (e) => {
     `<div class='list-img'>${img}</div>`
   );
 
-  // Scroll to the list of images
   const listImg = document.querySelector(".list-img");
   listImg.scrollIntoView({ behavior: "smooth", block: "start" });
 
-  // Add a click event listener to reset the form
   const resetBtn = document.querySelector(".reset");
   resetBtn.addEventListener("click", () => {
     urlInput.value = "";
   });
 
-  // Check if the page is scrolled and show the back to top button
   const height = document.documentElement.scrollHeight;
   const backToTopBtn = document.querySelector(".backToTop");
   backToTopBtn.style.display = height > 1000 ? "block" : "none";
 
-  // Add a click event listener to scroll to the top
   backToTopBtn.addEventListener("click", () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
     backToTopBtn.style.display = "none";
