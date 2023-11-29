665cf1ebb0f3a17c26ffd4905dfa920e29230eab
Yuu <122996864+Id-Yuu@users.noreply.github.com>
2023-11-29 16:11:02 +0700

    refactorAny

--- main.js
+++ main.js
@@ -1,49 +1,74 @@
-const formsData = async (e) => {
-  e.preventDefault();
+    const formid = document.getElementById("forms");
+    const getInput = document.querySelector("#input");
+    const reset = document.querySelector("#reset");
 
-  const urlInput = document.getElementById("url");
-  const url = urlInput.value.trim();
+    formid.addEventListener("submit", (e) => {
+      e.preventDefault();
 
-  const isValidUrl = url.includes("watch?v=") && url.length >= 32;
-  const isMobileUrl =
-    url.includes("m.youtube.com/watch?v=") && url.length >= 37;
-  const isValidUrl2 = url.includes("youtube.com/watch?v=") && url.length >= 32;
+      const inputValue = getInput.value;
 
-  const params = url.split("watch?v=")[1];
+      const url = new URL(inputValue);
+      const urlSearchParams = new URLSearchParams(url.search);
+      const param = urlSearchParams.get("v");
+      const path = url.pathname.slice(1);
 
-  if (isMobileUrl || isValidUrl2) {
-    params.split("&")[0];
-  } else if (!isValidUrl) {
-    alert("Invalid URL");
-  }
+      if (inputValue === "") {
+        getInput.value = "Oops Wrong URL";
+      } else {
+        const imageContainer = document.querySelector(".container-image");
 
-  const imageUrls = [
-    `https://img.youtube.com/vi/${params}/maxresdefault.jpg`,
-    `https://img.youtube.com/vi/${params}/sddefault.jpg`,
-    `https://img.youtube.com/vi/${params}/hqdefault.jpg`,
-  ];
+        const intervalId = setInterval(() => {
+          try {
+            const imageUrls = generateImageUrls(param ? param : path);
 
-  const outputTitle = document.getElementById("outputTitle");
-  const img = imageUrls.map((url) => `<img src="${url}">`).join("");
-  outputTitle.insertAdjacentHTML(
-    "afterend",
-    `<div class='list-img'>${img}</div>`
-  );
+            for (let i = 0; i < imageUrls.length; i++) {
+              const image = document.createElement("img");
+              const containerDiv = document.createElement("div");
+              containerDiv.classList.add("image");
+              const imageUrl = imageUrls[i];
+              image.src = imageUrl;
+              containerDiv.appendChild(image);
+              imageContainer.appendChild(containerDiv);
+            }
 
-  const listImg = document.querySelector(".list-img");
-  listImg.scrollIntoView({ behavior: "smooth", block: "start" });
+            imageContainer.scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+            });
 
-  const resetBtn = document.querySelector(".reset");
-  resetBtn.addEventListener("click", () => {
-    urlInput.value = "";
-  });
+            clearInterval(intervalId);
+          } catch (error) {
+            console.error("Error generating image URLs:", error);
+          }
+        }, 1000);
 
-  const height = document.documentElement.scrollHeight;
-  const backToTopBtn = document.querySelector(".backToTop");
-  backToTopBtn.style.display = height > 1000 ? "block" : "none";
+        reset.addEventListener("click", () => {
+          getInput.value = "";
+          imageContainer.querySelectorAll(".image").forEach((img) => {
+            img.remove();
+          });
+        });
+      }
 
-  backToTopBtn.addEventListener("click", () => {
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-    backToTopBtn.style.display = "none";
-  });
-};
+      const backToTopBtn = document.querySelector(".backTop");
+      const height = document.documentElement.scrollHeight;
+      backToTopBtn.style.display = height > 1000 ? "block" : "none";
+
+      backToTopBtn.addEventListener("click", () => {
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+        backToTopBtn.style.display = "none";
+      });
+      // END SUBMIT
+    });
+
+    function generateImageUrls(value) {
+      if (!value) {
+        throw new Error("Missing video parameter value");
+      }
+
+      return [
+        `https://img.youtube.com/vi/${value}/maxresdefault.jpg`,
+        `https://img.youtube.com/vi/${value}/sddefault.jpg`,
+        `https://img.youtube.com/vi/${value}/hqdefault.jpg`,
+      ];
+    }
